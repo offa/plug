e4e89bf1e27f9ead8ae597405a2e6a32f3891e89
piorekf <piorekf@gmail.com>
2011-02-01 19:43:45 +0100

    Added keyboard shortcuts - fixes issue 9

--- plug/effect.cpp
+++ plug/effect.cpp
@@ -238,52 +238,52 @@ void Effect::choose_fx(int value)
         break;
 
     case OVERDRIVE:
-        ui->label->setText(tr("Level"));
-        ui->label_2->setText(tr("Gain"));
-        ui->label_3->setText(tr("Low"));
-        ui->label_4->setText(tr("Medium"));
-        ui->label_5->setText(tr("High"));
+        ui->label->setText(tr("&Level"));
+        ui->label_2->setText(tr("&Gain"));
+        ui->label_3->setText(tr("L&ow"));
+        ui->label_4->setText(tr("&Medium"));
+        ui->label_5->setText(tr("&High"));
         ui->label_6->setText(tr(""));
         break;
 
     case FIXED_WAH:
-        ui->label->setText(tr("Level"));
-        ui->label_2->setText(tr("Frequency"));
-        ui->label_3->setText(tr("Min Freq"));
-        ui->label_4->setText(tr("Max Freq"));
-        ui->label_5->setText(tr("Q"));
+        ui->label->setText(tr("&Level"));
+        ui->label_2->setText(tr("&Frequency"));
+        ui->label_3->setText(tr("Mi&n Freq"));
+        ui->label_4->setText(tr("Ma&x Freq"));
+        ui->label_5->setText(tr("&Q"));
         ui->label_6->setText(tr(""));
         break;
 
     case TOUCH_WAH:
-        ui->label->setText(tr("Level"));
-        ui->label_2->setText(tr("Sensivity"));
-        ui->label_3->setText(tr("Min Freq"));
-        ui->label_4->setText(tr("Max Freq"));
-        ui->label_5->setText(tr("Q"));
+        ui->label->setText(tr("&Level"));
+        ui->label_2->setText(tr("&Sensivity"));
+        ui->label_3->setText(tr("Mi&n Freq"));
+        ui->label_4->setText(tr("Ma&x Freq"));
+        ui->label_5->setText(tr("&Q"));
         ui->label_6->setText(tr(""));
         break;
 
     case FUZZ:
-        ui->label->setText(tr("Level"));
-        ui->label_2->setText(tr("Gain"));
-        ui->label_3->setText(tr("Octave"));
-        ui->label_4->setText(tr("Low"));
-        ui->label_5->setText(tr("High"));
+        ui->label->setText(tr("&Level"));
+        ui->label_2->setText(tr("&Gain"));
+        ui->label_3->setText(tr("&Octave"));
+        ui->label_4->setText(tr("L&ow"));
+        ui->label_5->setText(tr("&High"));
         ui->label_6->setText(tr(""));
         break;
 
     case FUZZ_TOUCH_WAH:
-        ui->label->setText(tr("Level"));
-        ui->label_2->setText(tr("Gain"));
-        ui->label_3->setText(tr("Sensivity"));
-        ui->label_4->setText(tr("Octave"));
-        ui->label_5->setText(tr("Peak"));
+        ui->label->setText(tr("&Level"));
+        ui->label_2->setText(tr("&Gain"));
+        ui->label_3->setText(tr("&Sensivity"));
+        ui->label_4->setText(tr("&Octave"));
+        ui->label_5->setText(tr("&Peak"));
         ui->label_6->setText(tr(""));
         break;
 
     case SIMPLE_COMP:
-        ui->label->setText(tr("Type"));
+        ui->label->setText(tr("&Type"));
         ui->label_2->setText(tr(""));
         ui->label_3->setText(tr(""));
         ui->label_4->setText(tr(""));
@@ -292,144 +292,144 @@ void Effect::choose_fx(int value)
         break;
 
     case COMPRESSOR:
-        ui->label->setText(tr("Level"));
-        ui->label_2->setText(tr("Threshold"));
-        ui->label_3->setText(tr("Ratio"));
-        ui->label_4->setText(tr("Attack"));
-        ui->label_5->setText(tr("Release"));
+        ui->label->setText(tr("&Level"));
+        ui->label_2->setText(tr("&Threshold"));
+        ui->label_3->setText(tr("&Ratio"));
+        ui->label_4->setText(tr("Atta&ck"));
+        ui->label_5->setText(tr("&Release"));
         ui->label_6->setText(tr(""));
         break;
 
     case SINE_CHORUS:
     case TRIANGLE_CHORUS:
-        ui->label->setText(tr("Level"));
-        ui->label_2->setText(tr("Rate"));
-        ui->label_3->setText(tr("Depth"));
-        ui->label_4->setText(tr("Avr Delay"));
-        ui->label_5->setText(tr("LR Phase"));
+        ui->label->setText(tr("&Level"));
+        ui->label_2->setText(tr("&Rate"));
+        ui->label_3->setText(tr("&Depth"));
+        ui->label_4->setText(tr("A&vr Delay"));
+        ui->label_5->setText(tr("LR &Phase"));
         ui->label_6->setText(tr(""));
         break;
 
     case SINE_FLANGER:
     case TRIANGLE_FLANGER:
-        ui->label->setText(tr("Level"));
-        ui->label_2->setText(tr("Rate"));
-        ui->label_3->setText(tr("Depth"));
-        ui->label_4->setText(tr("Feedback"));
-        ui->label_5->setText(tr("LR Phase"));
+        ui->label->setText(tr("&Level"));
+        ui->label_2->setText(tr("&Rate"));
+        ui->label_3->setText(tr("&Depth"));
+        ui->label_4->setText(tr("&Feedback"));
+        ui->label_5->setText(tr("LR &Phase"));
         ui->label_6->setText(tr(""));
         break;
 
     case VIBRATONE:
-        ui->label->setText(tr("Level"));
-        ui->label_2->setText(tr("Rotor"));
-        ui->label_3->setText(tr("Depth"));
-        ui->label_4->setText(tr("Feedback"));
-        ui->label_5->setText(tr("LR Phase"));
+        ui->label->setText(tr("&Level"));
+        ui->label_2->setText(tr("&Rotor"));
+        ui->label_3->setText(tr("&Depth"));
+        ui->label_4->setText(tr("&Feedback"));
+        ui->label_5->setText(tr("LR &Phase"));
         ui->label_6->setText(tr(""));
         break;
 
     case VINTAGE_TREMOLO:
-        ui->label->setText(tr("Level"));
-        ui->label_2->setText(tr("Rate"));
-        ui->label_3->setText(tr("Duty Cycle"));
-        ui->label_4->setText(tr("Attack"));
-        ui->label_5->setText(tr("Release"));
+        ui->label->setText(tr("&Level"));
+        ui->label_2->setText(tr("&Rate"));
+        ui->label_3->setText(tr("&Duty Cycle"));
+        ui->label_4->setText(tr("Atta&ck"));
+        ui->label_5->setText(tr("Relea&se"));
         ui->label_6->setText(tr(""));
         break;
 
     case SINE_TREMOLO:
-        ui->label->setText(tr("Level"));
-        ui->label_2->setText(tr("Rate"));
-        ui->label_3->setText(tr("Duty Cycle"));
-        ui->label_4->setText(tr("LFO Clipping"));
-        ui->label_5->setText(tr("Tri Shaping"));
+        ui->label->setText(tr("&Level"));
+        ui->label_2->setText(tr("&Rate"));
+        ui->label_3->setText(tr("&Duty Cycle"));
+        ui->label_4->setText(tr("LFO &Clipping"));
+        ui->label_5->setText(tr("&Tri Shaping"));
         ui->label_6->setText(tr(""));
         break;
 
     case RING_MODULATOR:
-        ui->label->setText(tr("Level"));
-        ui->label_2->setText(tr("Frequency"));
-        ui->label_3->setText(tr("Depth"));
-        ui->label_4->setText(tr("LFO Shape"));
-        ui->label_5->setText(tr("LFO Phase"));
+        ui->label->setText(tr("&Level"));
+        ui->label_2->setText(tr("&Frequency"));
+        ui->label_3->setText(tr("&Depth"));
+        ui->label_4->setText(tr("LFO &Shape"));
+        ui->label_5->setText(tr("LFO &Phase"));
         ui->label_6->setText(tr(""));
         break;
 
     case STEP_FILTER:
-        ui->label->setText(tr("Level"));
-        ui->label_2->setText(tr("Rate"));
-        ui->label_3->setText(tr("Resonance"));
-        ui->label_4->setText(tr("Min Freq"));
-        ui->label_5->setText(tr("Max Freq"));
+        ui->label->setText(tr("&Level"));
+        ui->label_2->setText(tr("&Rate"));
+        ui->label_3->setText(tr("Re&sonance"));
+        ui->label_4->setText(tr("Mi&n Freq"));
+        ui->label_5->setText(tr("Ma&x Freq"));
         ui->label_6->setText(tr(""));
         break;
 
     case PHASER:
-        ui->label->setText(tr("Level"));
-        ui->label_2->setText(tr("Rate"));
-        ui->label_3->setText(tr("Depth"));
-        ui->label_4->setText(tr("Feedback"));
-        ui->label_5->setText(tr("LFO Shape"));
+        ui->label->setText(tr("&Level"));
+        ui->label_2->setText(tr("&Rate"));
+        ui->label_3->setText(tr("&Depth"));
+        ui->label_4->setText(tr("&Feedback"));
+        ui->label_5->setText(tr("LFO &Shape"));
         ui->label_6->setText(tr(""));
         break;
 
     case PITCH_SHIFTER:
-        ui->label->setText(tr("Level"));
-        ui->label_2->setText(tr("Pitch"));
-        ui->label_3->setText(tr("Detune"));
-        ui->label_4->setText(tr("Feedback"));
-        ui->label_5->setText(tr("Predelay"));
+        ui->label->setText(tr("&Level"));
+        ui->label_2->setText(tr("&Pitch"));
+        ui->label_3->setText(tr("&Detune"));
+        ui->label_4->setText(tr("&Feedback"));
+        ui->label_5->setText(tr("P&redelay"));
         ui->label_6->setText(tr(""));
         break;
 
     case MONO_ECHO_FILTER:
     case STEREO_ECHO_FILTER:
-        ui->label->setText(tr("Level"));
-        ui->label_2->setText(tr("Delay"));
-        ui->label_3->setText(tr("Feedback"));
-        ui->label_4->setText(tr("Frequency"));
-        ui->label_5->setText(tr("Ressonance"));
-        ui->label_6->setText(tr("In Level"));
+        ui->label->setText(tr("&Level"));
+        ui->label_2->setText(tr("&Delay"));
+        ui->label_3->setText(tr("&Feedback"));
+        ui->label_4->setText(tr("Fre&quency"));
+        ui->label_5->setText(tr("&Ressonance"));
+        ui->label_6->setText(tr("&In Level"));
         break;
 
     case MONO_DELAY:
     case MULTITAP_DELAY:
     case REVERSE_DELAY:
     case PING_PONG_DELAY:
-        ui->label->setText(tr("Level"));
-        ui->label_2->setText(tr("Delay"));
-        ui->label_3->setText(tr("Feedback"));
-        ui->label_4->setText(tr("Brightness"));
-        ui->label_5->setText(tr("Attenuation"));
+        ui->label->setText(tr("&Level"));
+        ui->label_2->setText(tr("&Delay"));
+        ui->label_3->setText(tr("&Feedback"));
+        ui->label_4->setText(tr("&Brightness"));
+        ui->label_5->setText(tr("A&ttenuation"));
         ui->label_6->setText(tr(""));
         break;
 
     case DUCKING_DELAY:
-        ui->label->setText(tr("Level"));
-        ui->label_2->setText(tr("Delay"));
-        ui->label_3->setText(tr("Feedback"));
-        ui->label_4->setText(tr("Release"));
-        ui->label_5->setText(tr("Threshold"));
+        ui->label->setText(tr("&Level"));
+        ui->label_2->setText(tr("&Delay"));
+        ui->label_3->setText(tr("&Feedback"));
+        ui->label_4->setText(tr("&Release"));
+        ui->label_5->setText(tr("&Threshold"));
         ui->label_6->setText(tr(""));
         break;
 
     case TAPE_DELAY:
-        ui->label->setText(tr("Level"));
-        ui->label_2->setText(tr("Delay"));
-        ui->label_3->setText(tr("Feedback"));
-        ui->label_4->setText(tr("Flutter"));
-        ui->label_5->setText(tr("Brightness"));
-        ui->label_6->setText(tr("Stereo"));
+        ui->label->setText(tr("&Level"));
+        ui->label_2->setText(tr("&Delay"));
+        ui->label_3->setText(tr("&Feedback"));
+        ui->label_4->setText(tr("Fl&utter"));
+        ui->label_5->setText(tr("&Brightness"));
+        ui->label_6->setText(tr("&Stereo"));
         break;
 
     case STEREO_TAPE_DELAY:
-        ui->label->setText(tr("Level"));
-        ui->label_2->setText(tr("Delay"));
-        ui->label_3->setText(tr("Feedback"));
-        ui->label_4->setText(tr("Flutter"));
-        ui->label_5->setText(tr("Separation"));
-        ui->label_6->setText(tr("Brightness"));
+        ui->label->setText(tr("&Level"));
+        ui->label_2->setText(tr("&Delay"));
+        ui->label_3->setText(tr("&Feedback"));
+        ui->label_4->setText(tr("Fl&utter"));
+        ui->label_5->setText(tr("&Separation"));
+        ui->label_6->setText(tr("&Brightness"));
         break;
 
     case SMALL_HALL_REVERB:
@@ -442,11 +442,11 @@ void Effect::choose_fx(int value)
     case ARENA_REVERB:
     case FENDER_63_SPRING_REVERB:
     case FENDER_65_SPRING_REVERB:
-        ui->label->setText(tr("Level"));
-        ui->label_2->setText(tr("Decay"));
-        ui->label_3->setText(tr("Dwell"));
-        ui->label_4->setText(tr("Diffusion"));
-        ui->label_5->setText(tr("Tone"));
+        ui->label->setText(tr("&Level"));
+        ui->label_2->setText(tr("&Decay"));
+        ui->label_3->setText(tr("D&well"));
+        ui->label_4->setText(tr("D&iffusion"));
+        ui->label_5->setText(tr("&Tone"));
         ui->label_6->setText(tr(""));
         break;
     }
