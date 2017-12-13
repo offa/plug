8ae17ce27c9909f526959c658c3801c735c08c8b
offa <bm-dev@yandex.com>
2017-12-13 15:24:05 +0100

    License header added to all source files (#27).

--- include/amplifier.h
+++ include/amplifier.h
@@ -1,3 +1,24 @@
+/*
+ * PLUG - software to operate Fender Mustang amplifier
+ *        Linux replacement for Fender FUSE software
+ *
+ * Copyright (C) 2017  offa
+ * Copyright (C) 2010-2016  piorekf <piorek@piorekf.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef AMPLIFIER_H
 #define AMPLIFIER_H
 
