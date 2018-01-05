d33d6b7bb99f84ca85619da6296200106883cd06
offa <bm-dev@yandex.com>
2018-01-05 23:12:33 +0100

    CI Build script updated to take (optional) build arguments.

--- script/ci_build.sh
+++ script/ci_build.sh
@@ -2,12 +2,43 @@
 
 set -ex
 
+
+BUILD_ARGS="-DCMAKE_BUILD_TYPE=Debug"
+COVERAGE=false
+
+for arg in "$@"
+do
+    case "${arg}" in
+        -asan)
+            BUILD_ARGS="${BUILD_ARGS} -DSANITZIER_ASAN=ON"
+            ;;
+        -ubsan)
+            BUILD_ARGS="${BUILD_ARGS} -DSANITZIER_UBSAN=ON"
+            ;;
+        -cov)
+            BUILD_ARGS="${BUILD_ARGS} -DCOVERAGE=ON"
+            COVERAGE=true;
+            ;;
+    esac
+done
+
+
+
+
+
 if [[ "${CXX}" == clang* ]]
 then
     export CXXFLAGS="-stdlib=libc++"
 fi
 
 mkdir build && cd build
-cmake -DCMAKE_BUILD_TYPE=Debug ..
+cmake ${BUILD_ARGS} ..
 make
 make unittest
+
+
+if [[ "${COVERAGE}" == true ]]
+then
+    make coverage
+fi
+
