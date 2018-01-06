f19c49da4804a4ae275876a49de72d81066f7da2
offa <bm-dev@yandex.com>
2018-01-06 16:03:02 +0100

    Another approach to add Sanitizer flags.

--- cmake/Sanitizer.cmake
+++ cmake/Sanitizer.cmake
@@ -2,7 +2,6 @@
 macro(enable_sanitizer san)
     set(SAN_FLAG "-fsanitize=${san}")
     add_compile_options(${SAN_FLAG})
-    set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} ${SAN_FLAG}")
 endmacro()
 
 
