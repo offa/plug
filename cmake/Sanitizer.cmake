297d98ce6a248447aae26dbc5baa10ee39037120
offa <bm-dev@yandex.com>
2018-01-06 15:30:33 +0100

    Missing linker flags for the sanitizers added (#37).

--- cmake/Sanitizer.cmake
+++ cmake/Sanitizer.cmake
@@ -1,9 +1,11 @@
 
 if( SANITIZER_ASAN )
     add_compile_options(-fsanitize=address)
+    set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -fsanitize=address")
 endif()
 
 if( SANITIZER_UBSAN )
     add_compile_options(-fsanitize=undefined)
+    set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -fsanitize=undefined")
 endif()
 
