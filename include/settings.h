82724f0c390db4ddda1035d16f5256688aa69ec5
offa <bm-dev@yandex.com>
2018-01-08 13:32:48 +0100

    Obsolete setting according to the removed updater (#22) cleaned up.

--- include/settings.h
+++ include/settings.h
@@ -41,7 +41,6 @@ public:
     explicit Settings(QWidget *parent = nullptr);
 
 private slots:
-    void change_updates(bool);
     void change_connect(bool);
     void change_oneset(bool);
     void change_keepopen(bool);
--- src/settings.cpp
+++ src/settings.cpp
@@ -32,26 +32,17 @@ Settings::Settings(QWidget *parent) : QDialog(parent),
 
     ui->setupUi(this);
 
-    ui->checkBox->setChecked(settings.value("Settings/checkForUpdates").toBool());
     ui->checkBox_2->setChecked(settings.value("Settings/connectOnStartup").toBool());
     ui->checkBox_3->setChecked(settings.value("Settings/oneSetToSetThemAll").toBool());
     ui->checkBox_4->setChecked(settings.value("Settings/keepWindowsOpen").toBool());
     ui->checkBox_5->setChecked(settings.value("Settings/popupChangedWindows").toBool());
     ui->checkBox_6->setChecked(settings.value("Settings/defaultEffectValues").toBool());
 
-    connect(ui->checkBox, SIGNAL(toggled(bool)), this, SLOT(change_updates(bool)));
     connect(ui->checkBox_2, SIGNAL(toggled(bool)), this, SLOT(change_connect(bool)));
     connect(ui->checkBox_3, SIGNAL(toggled(bool)), this, SLOT(change_oneset(bool)));
     connect(ui->checkBox_4, SIGNAL(toggled(bool)), this, SLOT(change_keepopen(bool)));
     connect(ui->checkBox_5, SIGNAL(toggled(bool)), this, SLOT(change_popupwindows(bool)));
     connect(ui->checkBox_6, SIGNAL(toggled(bool)), this, SLOT(change_effectvalues(bool)));
-}
-
-void Settings::change_updates(bool value)
-{
-    QSettings settings;
-
-    settings.setValue("Settings/checkForUpdates", value);
 }
 
 void Settings::change_connect(bool value)
