3808ac50fe0f9ac167dbdbc70bcca1d2cdf8e8f2
offa <bm-dev@yandex.com>
2018-02-03 18:09:18 +0100

    Clang format includes subdirectories now (fixes #69).

--- cmake/ClangFormat.cmake
+++ cmake/ClangFormat.cmake
@@ -4,11 +4,11 @@ find_program(CLANG_FORMAT clang-format DOC "Clang Format executable")
 if( CLANG_FORMAT )
 
     file(GLOB_RECURSE FORMAT_SRC_FILES
-            "${PROJECT_SOURCE_DIR}/include/*.h"
-            "${PROJECT_SOURCE_DIR}/src/*.h"
-            "${PROJECT_SOURCE_DIR}/src/*.cpp"
-            "${PROJECT_SOURCE_DIR}/test/*.h"
-            "${PROJECT_SOURCE_DIR}/test/*.cpp"
+            "${PROJECT_SOURCE_DIR}/include/**.h"
+            "${PROJECT_SOURCE_DIR}/src/**.h"
+            "${PROJECT_SOURCE_DIR}/src/**.cpp"
+            "${PROJECT_SOURCE_DIR}/test/**.h"
+            "${PROJECT_SOURCE_DIR}/test/**.cpp"
             )
 
     add_custom_target(clang-format COMMAND ${CLANG_FORMAT} -i ${FORMAT_SRC_FILES})
