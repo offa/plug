cb9339dcefb0fe0c3fe842fe8d3d38a947cb5237
offa <bm-dev@yandex.com>
2018-03-29 20:04:34 +0200

    Container-Overflow reported by Clang 6 fixed. The cause was GTest's ElementsAre(...) which has been replaced to fix the issue (#97).

--- test/UsbCommTest.cpp
+++ test/UsbCommTest.cpp
@@ -275,6 +275,7 @@ TEST_F(UsbCommTest, interruptReadResizesBufferOnPartialTransfer)
     setupHandle();
 
     const std::vector<std::uint8_t> data{0, 1, 2, 3, 4, 5, 6};
+    const std::vector<std::uint8_t> expected{0, 1, 2, 3};
     const std::size_t readSize = data.size();
     constexpr std::size_t actualSize{4};
     constexpr std::uint8_t endpoint{0x81};
@@ -284,5 +285,5 @@ TEST_F(UsbCommTest, interruptReadResizesBufferOnPartialTransfer)
         .WillOnce(DoAll(SetArrayArgument<2>(data.cbegin(), std::next(data.cbegin(), actualSize)), SetArgPointee<4>(actualSize), Return(0)));
 
     const auto buffer = comm->interruptReceive(endpoint, readSize);
-    EXPECT_THAT(buffer, ElementsAre(0, 1, 2, 3));
+    EXPECT_THAT(buffer, Eq(expected));
 }
