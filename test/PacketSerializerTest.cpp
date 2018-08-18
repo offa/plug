c3234dfa4e57682ad69d6a11f46e3d560ac1c73e
offa <bm-dev@yandex.com>
2018-08-18 21:47:37 +0200

    PacketSerializer Testsuite added.

---
+++ test/PacketSerializerTest.cpp
@@ -0,0 +1,46 @@
+/*
+ * PLUG - software to operate Fender Mustang amplifier
+ *        Linux replacement for Fender FUSE software
+ *
+ * Copyright (C) 2017-2018  offa
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "com/PacketSerializer.h"
+#include <gmock/gmock.h>
+
+using namespace plug::com;
+using namespace testing;
+
+class PacketSerializerTest : public testing::Test
+{
+protected:
+    void SetUp() override
+    {
+    }
+    void TearDown() override
+    {
+    }
+};
+
+TEST_F(PacketSerializerTest, serializeApplyCommand)
+{
+    Packet expected{};
+    expected[0] = 0x1c;
+    expected[1] = 0x03;
+    const auto packet = serializeApplyCommand();
+    EXPECT_THAT(packet, ElementsAreArray(expected));
+}
+
