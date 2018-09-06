46177faef122f3860f602300b6d61a8fcf93090a
offa <bm-dev@yandex.com>
2018-09-06 20:35:14 +0200

    Packet matchers extracted to header.

--- test/PacketSerializerTest.cpp
+++ test/PacketSerializerTest.cpp
@@ -20,49 +20,13 @@
 
 #include "com/PacketSerializer.h"
 #include "data_structs.h"
+#include "matcher/PacketMatcher.h"
 #include <gmock/gmock.h>
 
 using namespace plug;
 using namespace plug::com;
 using namespace testing;
-
-MATCHER_P4(AmpDataIs, ampId, v0, v1, v2, "")
-{
-    const std::tuple actual{arg[AMPLIFIER], arg[40], arg[43], arg[44], arg[45], arg[46], arg[50], arg[54]};
-    const auto [a0, a1, a2, a3, a4, a5, a6, a7] = actual;
-    *result_listener << " with amp specific values: ("
-                     << int{a0} << ", {" << int{a1} << ", " << int{a2} << "}, {"
-                     << int{a3} << ", " << int{a4} << ", " << int{a5} << ", " << int{a6}
-                     << "}, " << int{a7} << ")";
-    return std::tuple{ampId, v0, v0, v1, v1, v1, v1, v2} == actual;
-}
-
-MATCHER_P(CabinetDataIs, cabinetValue, "")
-{
-    const auto actual = arg[CABINET];
-    *result_listener << " with cabinet data: " << int{actual};
-    return actual == cabinetValue;
-}
-
-MATCHER_P4(EffectDataIs, dsp, effect, v0, v1, "")
-{
-    const std::tuple actual{arg[DSP], arg[EFFECT], arg[19], arg[20]};
-    const auto [a0, a1, a2, a3] = actual;
-    *result_listener << " with effect values: (" << int{a0} << ", " << int{a1}
-                     << ", " << int{a2} << ", " << int{a3} << ")";
-
-    return std::tuple{dsp, effect, v0, v1} == actual;
-}
-
-MATCHER_P6(KnobsAre, k1, k2, k3, k4, k5, k6, "")
-{
-    const std::tuple actual{arg[KNOB1], arg[KNOB2], arg[KNOB3],
-                            arg[KNOB4], arg[KNOB5], arg[KNOB6]};
-    const auto [a1, a2, a3, a4, a5, a6] = actual;
-    *result_listener << " with knobs: (" << int{a1} << ", " << int{a2} << ", " << int{a3}
-                     << ", " << int{a4} << ", " << int{a5} << ", " << int{a6} << ")";
-    return std::tuple{k1, k2, k3, k4, k5, k6} == actual;
-}
+using namespace test::matcher;
 
 
 class PacketSerializerTest : public testing::Test
---
+++ test/matcher/PacketMatcher.h
@@ -0,0 +1,70 @@
+/*
+ * PLUG - software to operate Fender Mustang amplifier
+ *        Linux replacement for Fender FUSE software
+ *
+ * Copyright (C) 2017-2018  offa
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "com/Packet.h"
+#include "com/MustangConstants.h"
+#include "data_structs.h"
+#include <tuple>
+#include <gmock/gmock.h>
+
+
+namespace test::matcher
+{
+MATCHER_P4(AmpDataIs, ampId, v0, v1, v2, "")
+{
+    const std::tuple actual{arg[plug::com::AMPLIFIER], arg[40], arg[43], arg[44], arg[45], arg[46], arg[50], arg[54]};
+    const auto [a0, a1, a2, a3, a4, a5, a6, a7] = actual;
+    *result_listener << " with amp specific values: ("
+                     << int{a0} << ", {" << int{a1} << ", " << int{a2} << "}, {"
+                     << int{a3} << ", " << int{a4} << ", " << int{a5} << ", " << int{a6}
+                     << "}, " << int{a7} << ")";
+    return std::tuple{ampId, v0, v0, v1, v1, v1, v1, v2} == actual;
+}
+
+MATCHER_P(CabinetDataIs, cabinetValue, "")
+{
+    const auto actual = arg[plug::com::CABINET];
+    *result_listener << " with cabinet data: " << int{actual};
+    return actual == cabinetValue;
+}
+
+MATCHER_P4(EffectDataIs, dsp, effect, v0, v1, "")
+{
+    const std::tuple actual{arg[plug::com::DSP], arg[plug::com::EFFECT], arg[19], arg[20]};
+    const auto [a0, a1, a2, a3] = actual;
+    *result_listener << " with effect values: (" << int{a0} << ", " << int{a1}
+                     << ", " << int{a2} << ", " << int{a3} << ")";
+
+    return std::tuple{dsp, effect, v0, v1} == actual;
+}
+
+MATCHER_P6(KnobsAre, k1, k2, k3, k4, k5, k6, "")
+{
+    const std::tuple actual{arg[plug::com::KNOB1], arg[plug::com::KNOB2], arg[plug::com::KNOB3],
+                            arg[plug::com::KNOB4], arg[plug::com::KNOB5], arg[plug::com::KNOB6]};
+    const auto [a1, a2, a3, a4, a5, a6] = actual;
+    *result_listener << " with knobs: (" << int{a1} << ", " << int{a2} << ", " << int{a3}
+                     << ", " << int{a4} << ", " << int{a5} << ", " << int{a6} << ")";
+    return std::tuple{k1, k2, k3, k4, k5, k6} == actual;
+}
+
+}
