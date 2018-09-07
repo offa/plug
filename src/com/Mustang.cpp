c3cf3e9c1877f47bed3c72937ee4c01a60d8567b
offa <bm-dev@yandex.com>
2018-09-07 18:23:54 +0200

    Refactoring; new function used for serialization of the apply command with fx knob.

--- src/com/Mustang.cpp
+++ src/com/Mustang.cpp
@@ -140,10 +140,7 @@ namespace plug::com
         const auto packets = serializeSaveEffectPacket(slot, effects);
         std::for_each(packets.cbegin(), packets.cend(), [this](const auto& p) { sendCommand(p); });
 
-        Packet applyCommand = serializeApplyCommand();
-        applyCommand[FXKNOB] = getFxKnob(effects[0]);
-
-        sendCommand(applyCommand);
+        sendCommand(serializeApplyCommand(getFxKnob(effects[0])));
     }
 
     void Mustang::loadInitialData(char list[][32], char* name, amp_settings* amp_set, fx_pedal_settings* effects_set)
