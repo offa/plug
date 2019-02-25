d0ab863a32c2b0cf7e2507148ca6be21e2763fca
offa <bm-dev@yandex.com>
2019-02-25 17:15:43 +0100

    Connection Factory added (#153).

---
+++ src/com/ConnectionFactory.cpp
@@ -0,0 +1,44 @@
+/*
+ * PLUG - software to operate Fender Mustang amplifier
+ *        Linux replacement for Fender FUSE software
+ *
+ * Copyright (C) 2017-2019  offa
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "com/ConnectionFactory.h"
+#include "com/UsbComm.h"
+#include "com/MustangConstants.h"
+
+namespace plug::com
+{
+    namespace {
+        inline constexpr std::initializer_list<std::uint16_t> pids{
+            SMALL_AMPS_USB_PID,
+            BIG_AMPS_USB_PID,
+            SMALL_AMPS_V2_USB_PID,
+            BIG_AMPS_V2_USB_PID,
+            MINI_USB_PID,
+            FLOOR_USB_PID};
+    }
+
+    std::shared_ptr<Connection> createUsbConnection()
+    {
+        auto conn = std::make_shared<UsbComm>();
+        conn->openFirst(USB_VID, pids);
+        return conn;
+    }
+}
+
---
+++ test/ConnectionFactoryTest.cpp
@@ -0,0 +1,95 @@
+/*
+ * PLUG - software to operate Fender Mustang amplifier
+ *        Linux replacement for Fender FUSE software
+ *
+ * Copyright (C) 2017-2019  offa
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "com/ConnectionFactory.h"
+#include "com/CommunicationException.h"
+#include "com/MustangConstants.h"
+#include "mocks/LibUsbMocks.h"
+#include <gmock/gmock.h>
+
+using namespace plug::com;
+using namespace testing;
+
+class ConnectionFactoryTest : public testing::Test
+{
+protected:
+    void SetUp() override
+    {
+        usbmock = mock::resetUsbMock();
+        ignoreClose();
+    }
+
+    void TearDown() override
+    {
+        mock::clearUsbMock();
+    }
+
+    void ignoreClose()
+    {
+        EXPECT_CALL(*usbmock, release_interface(_, _)).Times(AnyNumber());
+        EXPECT_CALL(*usbmock, attach_kernel_driver(_, _)).Times(AnyNumber());
+        EXPECT_CALL(*usbmock, close(_)).Times(AnyNumber());
+        EXPECT_CALL(*usbmock, exit(_)).Times(AnyNumber());
+    }
+
+
+    mock::UsbMock* usbmock = nullptr;
+    libusb_device_handle handle{};
+    static inline constexpr std::uint16_t vid{USB_VID};
+};
+
+TEST_F(ConnectionFactoryTest, createUsbConnectionOpensDevice)
+{
+
+    InSequence s;
+    EXPECT_CALL(*usbmock, init(nullptr));
+    EXPECT_CALL(*usbmock, open_device_with_vid_pid(nullptr, vid, _)).WillOnce(Return(&handle));
+    EXPECT_CALL(*usbmock, kernel_driver_active(&handle, 0));
+    EXPECT_CALL(*usbmock, claim_interface(&handle, 0));
+
+    auto conn = createUsbConnection();
+    EXPECT_TRUE(conn->isOpen());
+}
+
+TEST_F(ConnectionFactoryTest, createUsbConnectionOpensFirstMatchedDevice)
+{
+    InSequence s;
+    EXPECT_CALL(*usbmock, init(nullptr));
+    EXPECT_CALL(*usbmock, open_device_with_vid_pid(nullptr, vid, _))
+        .Times(2)
+        .WillRepeatedly(Return(nullptr));
+    EXPECT_CALL(*usbmock, open_device_with_vid_pid(nullptr, vid, _)).WillOnce(Return(&handle));
+    EXPECT_CALL(*usbmock, kernel_driver_active(&handle, 0));
+    EXPECT_CALL(*usbmock, claim_interface(&handle, 0));
+
+    auto conn = createUsbConnection();
+    EXPECT_TRUE(conn->isOpen());
+}
+
+TEST_F(ConnectionFactoryTest, createUsbConnectionThrowsIfNoMatchingDeviceFound)
+{
+    InSequence s;
+    EXPECT_CALL(*usbmock, init(nullptr));
+    EXPECT_CALL(*usbmock, open_device_with_vid_pid(nullptr, vid, _))
+        .Times(AtLeast(1))
+        .WillRepeatedly(Return(nullptr));
+
+    EXPECT_THROW(createUsbConnection(), CommunicationException);
+}
