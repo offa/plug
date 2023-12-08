175d114da098709e4f4cb24f3cb2326cc5359fbf
offa <bm-dev@yandex.com>
2023-12-08 15:09:55 +0100

    Update GoogleTest used in CI to 1.14.0 (#280)

--- script/ci_setup.sh
+++ script/ci_setup.sh
@@ -15,7 +15,7 @@ apt-get install -y --no-install-recommends \
         qtbase5-dev \
         libusb-1.0-0-dev
 
-git clone --depth=1 --branch=release-1.12.1 https://github.com/google/googletest.git
+git clone --depth=1 --branch=v1.14.0 https://github.com/google/googletest.git
 mkdir googletest/build
 cd googletest/build
 cmake -DCMAKE_CXX_STANDARD=17 ..
